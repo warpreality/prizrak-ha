195c254132ede890eb6d2c2cd60813c04f2a120c
Maksim Kachynski <max.kachinsky@proton.me>
2025-12-14 12:09:53 +0300

    init

--- custom_components/prizrak/coordinator.py
+++ custom_components/prizrak/coordinator.py
@@ -8,6 +8,7 @@ from datetime import timedelta
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
 from .api import PrizrakAPI, PrizrakAPIError
@@ -23,7 +24,7 @@ class PrizrakDataUpdateCoordinator(DataUpdateCoordinator):
         """Инициализация координатора."""
         self.entry = entry
         self.api = PrizrakAPI(
-            hass.helpers.aiohttp_client.async_get_clientsession(),
+            async_get_clientsession(hass),
             entry.data[CONF_EMAIL],
             entry.data[CONF_PASSWORD],
         )
